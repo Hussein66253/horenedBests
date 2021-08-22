5c118348f6e7b01b0feaa2a8ca2f64328c72bf8c
hussein66253 <hussein66253@icloud.com>
2021-08-22 23:21:16 +0300

    b1&b2

--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,14 +1,38 @@
 import React from 'react';
-
+import { Card } from 'react-bootstrap'
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicks: 0
+        }
+    }
+
+    handelClicks = () => {
+        this.setState({
+            clicks: this.state.clicks + 1
+        })
+    }
     render() {
         return (
-            <div>
-                <h2>{this.props.data.title}</h2>
-                <img src={this.props.data.imgSrc} alt={this.props.data.imgAlt} title={this.props.data.title} />
-                <p>{this.props.data.description}</p>
-            </div>
+            // <div>
+
+            //     <h2>{this.props.title}</h2>
+            //     <img onClick={this.handelClicks} src={this.props.image_url} alt={this.props.title} title={this.props.title} />
+            //     <p>{this.props.description}</p>
+            //     <p>Number of clicks   {this.state.clicks} 🧡</p>
+            // </div>
+            <Card >
+                <Card.Img variant="top" onClick={this.handelClicks} src={this.props.image_url} alt={this.props.title} title={this.props.title} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Card.Footer>
+                        <small className="text-muted">Number of clicks   {this.state.clicks} 🧡</small>
+                    </Card.Footer>
+                </Card.Body>
+            </Card>
         )
     }
 }
- export default HornedBeast;
+export default HornedBeast;
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import DataJson from './data.json';
 class Main extends React.Component {
+
     render() {
         return (
             <div>
-                <HornedBeast
-                    data={{
-                        imgSrc: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-                        title: 'Uniwhal',
-                        description: "A unicorn and a narwhal nuzzling their horns",
-                    }} />
-                <HornedBeast
-                    data={{
-                        imgSrc: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-                        title: 'Uniwhal',
-                        description: "A unicorn and a narwhal nuzzling their horns",
-                    }} />
-                <HornedBeast
-                    data={{
-                        imgSrc: "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-                        title: 'Uniwhal',
-                        description: "A unicorn and a narwhal nuzzling their horns",
-                    }} />
+                {
+                    DataJson.map((item, index) => {
+                        return (
+                            <HornedBeast
 
-
+                                image_url={item.image_url}
+                                title={item.title}
+                                description={item.description}
+                            />
+                        )
+                    })
+                }
             </div>
         );
     }
